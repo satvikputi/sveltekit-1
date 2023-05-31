0b539ebc30369d56ce72b64566341613784507a4
satvikputi <134920060+satvikputi@users.noreply.github.com>
2023-05-31 18:49:50 +0200

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,26 +10,26 @@
 		This is a Rock Paper Scissor Game. Rock Paper Scissors is a simple hand game played between two people, often used as a way to make decisions or settle disputes in a fair manner. The game is based on a set of rules that determine the outcome of each round.
 	</p>
 
-	<pre>npm create svelte@latest</pre>
+	
 
 	<p>
-		Origin: The exact origin of Rock Paper Scissors is uncertain, as it has been played in various forms in different cultures throughout history. The game's origins can be traced back to ancient China, where it was known as "Shoushiling" or "Hand Command." It was believed to have been played as early as the Han dynasty (206 BC – 220 AD).
+		<b>Origin:</b> The exact origin of Rock Paper Scissors is uncertain, as it has been played in various forms in different cultures throughout history. The game's origins can be traced back to ancient China, where it was known as "Shoushiling" or "Hand Command." It was believed to have been played as early as the Han dynasty (206 BC – 220 AD).
 	</p>
 
 	<p>
-		Rules of the Game:
+		<b>Rules of the Game:</b>
 Rock Paper Scissors is typically played with two participants. The players simultaneously chant "Rock, Paper, Scissors" and then make a hand gesture representing one of the three options: rock, paper, or scissors.
 
-Rock: Form a fist with your hand.
-Paper: Extend your hand flat, with fingers together.
-Scissors: Extend your hand, separating your index and middle fingers to resemble a pair of scissors.
+		<b>Rock:</b> Form a fist with your hand.
+		<b>Paper:</b> Extend your hand flat, with fingers together.
+		<b>Scissors:</b> Extend your hand, separating your index and middle fingers to resemble a pair of scissors.
 The outcome of each round is determined by the following rules:
 
-Rock beats scissors: The rock smashes the scissors.
+<b>Rock beats scissors: The rock smashes the scissors.
 Scissors beats paper: The scissors cut the paper.
 Paper beats rock: The paper covers the rock.
 If both players make the same gesture, it results in a tie, and the game is usually replayed until a winner is determined. The game continues with multiple rounds until one player reaches a previously agreed-upon number of wins or until a specific objective is achieved.
-
+		</b>
 Rock Paper Scissors is a game that relies on chance, psychology, and quick decision-making. It has become a popular pastime and has even been studied in the context of game theory and human psychology.
 	</p>
 </div>
